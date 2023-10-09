ff6c855682e14ebfc1afc1111b5b331ad24e3b3a
diak <diak@mesterlevel.lo>
2023-10-09 12:33:06 +0200

    loading

--- lib/controllers/home_view_controller.dart
+++ lib/controllers/home_view_controller.dart
@@ -1,23 +1,44 @@
 import 'dart:math';
-
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:topjoy/constants.dart' as constants;
 
 class HomeViewController extends GetxController {
+  BuildContext context;
+  HomeViewController(this.context);
+
+bool isInitialized=false;
+
   int bottlecapState = 0;
   int random = 0;
   String giveMeMessage() {
     return (bottlecapState == 10)
-        ? constants.messages[random].toUpperCase(): "";
+        ? constants.messages[random].toUpperCase()
+        : "";
   }
-
   void flipTheBottlecap() async {
     bool condition = (bottlecapState == 0);
     random = Random().nextInt(constants.messages.length);
-    for (int i = (condition) ? 0 : 10;(condition) ? i < 11 : i >= 0;(condition) ? i++ : i--) {
+    for (int i = (condition) ? 0 : 10;
+        (condition) ? i < 11 : i >= 0;
+        (condition) ? i++ : i--) {
       bottlecapState = i;
       await Future.delayed(Duration(milliseconds: 15));
       update();
     }
+  }
+  Future<void> preloadAllImages() async {
+    for (int i = 0; i < 11; i++) {
+      await precacheImage(AssetImage('images/bottlecap_$i.png'), context);
+    }
+  }
+  @override
+  void onInit() async {
+    // TODO: implement onInit
+    super.onInit();
+    await preloadAllImages();
+    isInitialized = true;
+    // await Future.delayed(Duration(milliseconds: 2000));
+    update();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:topjoy/views/homeview.dart';
+import 'package:topjoy/views/home_view.dart';
 
 void main() {
   runApp(MaterialApp(
